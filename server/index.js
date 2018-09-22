6647054082cb1c2eb8d1881f5d53b7b1099d88a2
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-09-22 13:14:05 +1200

    setup files and package.json

---
+++ server/index.js
@@ -0,0 +1,11 @@
+var Knex = require('knex')
+var config = require('../knexfile')[process.env.NODE_ENV || 'development']
+var knex = Knex(config)
+
+var server = require('./server')
+server.set('db', knex)
+var PORT = process.env.PORT || 3000
+
+server.listen(PORT, function () {
+    console.log('Listening on port', PORT)
+})

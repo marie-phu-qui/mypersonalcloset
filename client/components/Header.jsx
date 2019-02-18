c1f953673fe2fc02a685ff05126444e5c31b6db0
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2019-02-18 14:52:17 +1300

    restore home page to display

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,8 +3,6 @@ import React from 'react'
 const Header = () => (
     <div id="main-title" class="col-lg-12">
         <h1>My personal closet</h1>
-
-        <p>lalalalalla</p>
         <hr align="center" width="70%"/>
     </div>
 )
